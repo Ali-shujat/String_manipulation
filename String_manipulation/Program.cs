c0917c4de1e295e09887f4ed41230470e5b5ce00
Shujat Ali <shuja4you@hotmail.com>
2020-05-13 14:33:07 +0100

    storing the result in another string

--- String_manipulation/Program.cs
+++ String_manipulation/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace String_manipulation
@@ -10,12 +11,36 @@ namespace String_manipulation
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Exersice 6 : String manipulation!");
+            // A program that asks the user for a string and:
+            //-Replaces all spaces with underscores(storing the result in a new string)
+            //- Removes leading spaces(storing the result in another string)
+            //- Removes trailing spaces(storing the result in another string)
+            //- Replaces all lowercase A by uppercase A(storing the result in another string)
+            //The program must display all the strings it generates.
+
+            Console.WriteLine("Please Enter a string!");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("User input string :" + str);
+
+            string strSpace = str.Replace(" ", "_");
+            Console.WriteLine("string with underscores :" + strSpace);
+
+            string strLspace = str.TrimStart();
+            Console.WriteLine("string without leading spaces :" + strLspace);
+
+            string strTspace = str.TrimEnd();
+            Console.WriteLine("string without trailing spaces :" + strTspace);
+
+            string strCase = str.Replace('a','A');
+            Console.WriteLine("string with lowercase A by uppercase A :" + strCase);
+
+
+
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
